7b7b6111b6e2534279e513b56630480b87c97871
blajnoy <blajnoy@gmail.com>
2017-04-24 18:55:28 +0300

    calendar

--- markup/js/main.js
+++ markup/js/main.js
@@ -2,56 +2,59 @@ $(document).ready(function () {
 
     /* schedule calendar */
 
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'prev',
-            center: 'title',
-            right: 'next'
-        },
-        defaultDate: '2014-06-12',
-        defaultView: 'month',
-        editable: false,
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2014-06-01',
-                description: 'This is a cool event'
+    if($('#calendar').length != 0) {
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev',
+                center: 'title',
+                right: 'next'
             },
-            {
-                title: 'Long Event',
-                start: '2014-06-07',
-                end: '2014-06-20'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2014-06-09T16:00:00'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2014-06-16T16:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2014-06-12T10:30:00',
-                end: '2014-06-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2014-06-12T12:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2014-06-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2014-06-28'
-            }
-        ]
-    });
+            defaultDate: '2014-06-12',
+            defaultView: 'month',
+            editable: false,
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2014-06-01',
+                    description: 'This is a cool event'
+                },
+                {
+                    title: 'Long Event',
+                    start: '2014-06-07',
+                    end: '2014-06-20'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2014-06-09T16:00:00'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2014-06-16T16:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2014-06-12T10:30:00',
+                    end: '2014-06-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2014-06-12T12:00:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2014-06-13T07:00:00'
+                },
+                {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2014-06-28'
+                }
+            ]
+        });
+    }
+
 
     /*$('#my-prev-button').click(function() {
         $('#calendar').fullCalendar('prev');
@@ -409,7 +412,7 @@ $(document).ready(function () {
                 _this.on('show.daterangepicker', function () {
                     dropDatapicker.position();
                 });
-                
+
                 _this.on('change.daterangepicker', function () {
                     dropDatapicker.position();
                 });
