8952bf318ee2a9573aad462de37b6431104ef4ff
blajnoy <blajnoy@gmail.com>
2017-05-25 18:40:55 +0300

    person counter

--- markup/public/js/single-page.js
+++ markup/public/js/single-page.js
@@ -5,34 +5,44 @@ $(document).ready(function () {
         'val': 11
     }];
 
-    var personExample = {
-        'min': 1,
-        'max': 20,
-        'val': 10
-    };
 
 
-    personCounter($('.persona'), personsDefault);
+    var elem = $('.persona');
 
-    function personCounter($element, persons, settings) {
+    persona1 = new PersonCounter({
+        element: elem,
+        persons: personsDefault
+    });
 
-        var action;
-        var that = this;
+    function PersonCounter(options) { //element, persons
 
-        that.persons = persons || [];
-        that.$wrapper = $element.after( '<div class="children-wrap"></div>' ).next('.children-wrap');
+        var $element = options.element;
+        var persons = options.persons || [];
+        var personExample = {
+            'min': 1,
+            'max': 20,
+            'val': 10
+        };
+        $element.after( '<div class="children-wrap"></div>' );
 
-        that.addPerson = function (person) {
-            that.persons.push(person);
-            that.render();
+        var $wrapper = $($element.next('.children-wrap'));
+
+        this.getPersons = function () {
+            return persons;
         };
 
-        that.removePerson = function (index) {
-            that.persons.splice(index, 1);
-            that.render();
-        };
+        function addPerson(person) {
 
-        that.render = function () {
+            persons.push(person);
+            render();
+        }
+
+        function removePerson(index) {
+            persons.splice(index, 1);
+            render();
+        }
+
+        function render() {
             var personsList = persons.map(function (person) {
                 return '<div class="item">' +
                     '<i class="gl-ico gl-ico-user"></i>' +
@@ -41,14 +51,15 @@ $(document).ready(function () {
                     '<input type="text" class="form-control text-center" value="' + person.val + '" min="' + person.min + '" max="' + person.max + '">' +
                     '<a href="javascript:void(0);" class="btn btn-link data-up" data-dir="up"><i class="gl-ico gl-ico-plus"></i></a>' +
                     '</div>' +
-                    '<a class="btn-add remove-child-item" href="javascript:void(0);"><i class="gl-ico gl-ico-cart-delete"></i></a>' +
+                    '<a class="remove-child-item" href="javascript:void(0);"><i class="gl-ico gl-ico-cart-delete"></i></a>' +
                     '</div>';
-
             });
 
-            that.$wrapper.html( personsList.join('') );
+            personsList = personsList.join('');
+            $wrapper.html( personsList );
 
             /* bind events */
+            var action;
 
             $(document).on('mousedown', ".number-spinner .btn", function () {
                     var btn = $(this);
@@ -64,11 +75,11 @@ $(document).ready(function () {
 
                                 if (icoUser.length != 0) {
                                     icoUser.css({
-                                        '-webkit-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-moz-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-ms-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-o-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        'transform': 'scale(' + that.stepIconRise(input.val()) + ')'
+                                        '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        'transform': 'scale(' + stepIconRise(input.val()) + ')'
                                     });
                                 }
 
@@ -84,11 +95,11 @@ $(document).ready(function () {
 
                                 if (icoUser.length != 0) {
                                     icoUser.css({
-                                        '-webkit-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-moz-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-ms-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        '-o-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                                        'transform': 'scale(' + that.stepIconRise(input.val()) + ')'
+                                        '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                                        'transform': 'scale(' + stepIconRise(input.val()) + ')'
                                     });
                                 }
 
@@ -99,10 +110,20 @@ $(document).ready(function () {
                         }, 70);
                     }
 
+                    var index = input.closest('.item').index();
+                    persons[index].val = parseInt(input.val());
+
                     return false;
                 })
                 .on('mouseup', ".number-spinner .btn", function () {
+                    var btn = $(this);
+                    var input = btn.closest('.number-spinner').find('input');
+
                     clearInterval(action);
+
+                    var index = input.closest('.item').index();
+                    persons[index].val = parseInt(input.val());
+
                     return false;
                 });
 
@@ -113,15 +134,18 @@ $(document).ready(function () {
                 var min = input.attr('min');
                 var max = input.attr('max');
 
-                input.val(that.minMax(value, min, max));
+                input.val(minMax(value, min, max));
+
+                var index = input.closest('.item').index();
+                persons[index].val = parseInt(input.val());
 
                 if (icoUser.length != 0) {
                     icoUser.css({
-                        '-webkit-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-moz-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-ms-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-o-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        'transform': 'scale(' + that.stepIconRise(input.val()) + ')'
+                        '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        'transform': 'scale(' + stepIconRise(input.val()) + ')'
                     });
                 }
 
@@ -131,45 +155,42 @@ $(document).ready(function () {
                 var icoUser = input.closest('.number-spinner').prev('.gl-ico-user');
 
                 if ($(this).val() == '') {
-
-
                     var min = input.attr('min');
-
                     input.val(min);
                 }
 
+                var index = input.closest('.item').index();
+                persons[index].val = parseInt(input.val());
+
                 if (icoUser.length != 0) {
                     icoUser.css({
-                        '-webkit-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-moz-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-ms-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        '-o-transform': 'scale(' + that.stepIconRise(input.val()) + ')',
-                        'transform': 'scale(' + that.stepIconRise(input.val()) + ')'
+                        '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
+                        'transform': 'scale(' + stepIconRise(input.val()) + ')'
                     });
                 }
             });
 
             $(document).on('click', ".remove-child-item", function (e) {
 
-                var index = index($(this).closest('.item'));
+                var elm = $(this).closest('.item');
+                var index = parseInt(elm.index());
 
-                that.removePerson(index);
+                removePerson(index);
 
                 e.stopImmediatePropagation();
 
             });
-        };
+        }
 
-        $(document).on('click', ".btn-add", function () {
-            that.addPerson(personExample);
-        });
-
-        that.stepIconRise = function(val) {
+        function stepIconRise(val) {
             var step = 1 + parseInt(val / 2) / 10;
             return step > 2 ? 2 : step;
-        };
+        }
 
-        that.minMax = function(value, min, max) {
+        function minMax(value, min, max) {
             if (value == '')
                 return value;
             else if (parseInt(value) < min || isNaN(parseInt(value)))
@@ -177,177 +198,14 @@ $(document).ready(function () {
             else if (parseInt(value) > max)
                 return max;
             else return value;
-        };
+        }
 
-        /**********/
-
-
-        that.addPerson(personExample);
-        that.removePerson(0, 1);
-    }
-
-
-    /**************************/
-
-    /*var action;
-    var childrenCount = 0;
-
-    $(document).on('mousedown', ".number-spinner .btn", function () {
-            var btn = $(this);
-            var input = btn.closest('.number-spinner').find('input');
-
-            var icoUser = btn.closest('.number-spinner').prev('.gl-ico-user');
-
-
-            btn.closest('.number-spinner').find('.btn').removeClass("disabled");
-
-
-            if (btn.attr('data-dir') == 'up') {
-                action = setInterval(function () {
-                    if (input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {
-                        input.val(parseInt(input.val()) + 1);
-
-                        if (icoUser.length != 0) {
-                            icoUser.css({
-                                '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                'transform': 'scale(' + stepIconRise(input.val()) + ')'
-                            });
-                        }
-
-                    } else {
-                        btn.addClass("disabled");
-                        clearInterval(action);
-                    }
-                }, 70);
-            } else {
-                action = setInterval(function () {
-                    if (input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {
-                        input.val(parseInt(input.val()) - 1);
-
-                        if (icoUser.length != 0) {
-                            icoUser.css({
-                                '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                                'transform': 'scale(' + stepIconRise(input.val()) + ')'
-                            });
-                        }
-
-                    } else {
-                        btn.addClass("disabled");
-                        clearInterval(action);
-                    }
-                }, 70);
-            }
-
-            return false;
-        })
-               .on('mouseup', ".number-spinner .btn", function () {
-
-            clearInterval(action);
-
-            return false;
+        $(document).on('click', ".btn-add", function () {
+            addPerson(Object.assign({}, personExample));
         });
 
-    $(document).on('keyup', ".number-spinner input", function () {
-        var input = $(this);
-        var icoUser = input.closest('.number-spinner').prev('.gl-ico-user');
-        var value = input.val();
-        var min = input.attr('min');
-        var max = input.attr('max');
+        render();
 
-        input.val(minmax(value, min, max));
-
-        if (icoUser.length != 0) {
-            icoUser.css({
-                '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                'transform': 'scale(' + stepIconRise(input.val()) + ')'
-            });
-        }
-
-    })
-               .on('blur', ".number-spinner input", function () {
-        var input = $(this);
-        var icoUser = input.closest('.number-spinner').prev('.gl-ico-user');
-
-        if ($(this).val() == '') {
-
-
-            var min = input.attr('min');
-
-            input.val(min);
-        }
-
-        if (icoUser.length != 0) {
-            icoUser.css({
-                '-webkit-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-moz-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-ms-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                '-o-transform': 'scale(' + stepIconRise(input.val()) + ')',
-                'transform': 'scale(' + stepIconRise(input.val()) + ')'
-            });
-        }
-    });
-
-    $(document).on('click', ".remove-chid-item", function (e) {
-
-        var item = $(this).closest('.item');
-
-        removeChildItem(item);
-        e.stopImmediatePropagation();
-
-    });
-
-    $(document).on('click', ".person-row .btn-add", function () {
-        var parent = $(this).closest('.person-row').next('.children-wrap');
-
-        addChildItem(parent, 1, 20)
-    });
-
-    function stepIconRise(val) {
-        var step = 1 + parseInt(val / 2) / 10;
-        return step > 2 ? 2 : step;
     }
-
-    function minmax(value, min, max) {
-        if (value == '')
-            return value;
-        else if (parseInt(value) < min || isNaN(parseInt(value)))
-            return min;
-        else if (parseInt(value) > max)
-            return max;
-        else return value;
-    }
-
-    function removeChildItem(item) {
-        item.remove();
-    }
-
-    function addChildItem(parent, min, max) {
-
-        var itemParent = parent;
-        var min = min || 1;
-        var max = max || 10;
-
-
-        var tpl = '<div class="item">' +
-            '<i class="gl-ico gl-ico-user"></i>' +
-            '<div class="number-spinner">' +
-            '<a href="javascript:void(0);" class="btn btn-link data-down" data-dir="dwn"><i class="gl-ico gl-ico-minus"></i></a>' +
-            '<input type="text" class="form-control text-center" value="1" min="' + min + '" max="' + max + '">' +
-            '<a href="javascript:void(0);" class="btn btn-link data-up" data-dir="up"><i class="gl-ico gl-ico-plus"></i></a>' +
-            '</div>' +
-            '<a class="btn-add remove-chid-item" href="javascript:void(0);"><i class="gl-ico gl-ico-cart-delete"></i></a>' +
-            '</div>';
-
-        itemParent.prepend($(tpl));
-    }*/
 });
 
